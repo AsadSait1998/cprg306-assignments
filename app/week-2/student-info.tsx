d02cdadc0ee97bcf39f2f30e4a24d5be6c1e3c29
Asad Arif <Asad.Arif@edu.sait.ca>
2026-01-20 14:32:46 -0700

    Create student-info.tsx

---
+++ app/week-2/student-info.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+        <div>
+            <h1>Student Information</h1>
+            <p>Name: Asad Arif</p>
+            <Link href="https://github.com/AsadSait1998" target="_blank" rel="noreferrer"><p>Github Profile : AsadSait1998 </p></Link>
+
+        </div>
+    )
+}
