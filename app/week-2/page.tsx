3eb83cb26a45e49f3f9f44e820b70b8166e87c84
Asad Arif <Asad.Arif@edu.sait.ca>
2026-01-20 14:38:18 -0700

    yes

--- app/week-2/page.tsx
+++ app/week-2/page.tsx
@@ -1,11 +1,7 @@
-import Link from "next/link";
-
 export default function Page() {
   return (
     <main>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
-
-      <Link href="/week-2">Go to Week 2</Link>
+      <h1>Shopping List</h1>
     </main>
   );
 }
