532a7c8a3ade4d9086f153b255169f5b4dd79f5c
Asad Arif <Asad.Arif@edu.sait.ca>
2026-01-27 13:37:37 -0700

    Done week-3

--- app/week-3/item-list.tsx
+++ app/week-3/item-list.tsx
@@ -2,18 +2,18 @@ import Item from "./item";
 
 export default function ItemList() {
   const items = [
-    { name: "milk, 4 L ", quantity: 1, category: "dairy" },
-    { name: "bread ", quantity: 2, category: "bakery" },
-    { name: "eggs, dozen ", quantity: 2, category: "dairy" },
-    { name: "bananas ", quantity: 6, category: "produce" },
-    { name: "broccoli ", quantity: 3, category: "produce" },
-    { name: "chicken breasts, 1 kg ", quantity: 1, category: "meat" },
-    { name: "pasta sauce ", quantity: 3, category: "canned goods" },
-    { name: "spaghetti, 454 g ", quantity: 2, category: "dry goods" },
-    { name: "toilet paper, 12 pack ", quantity: 1, category: "household" },
-    { name: "paper towels, 6 pack", quantity: 1, category: "household" },
-    { name: "dish soap ", quantity: 1, category: "household" },
-    { name: "hand soap ", quantity: 4, category: "household" },
+    { name: "Milk, 4 L ", quantity: 1, category: "dairy" },
+    { name: "Bread ", quantity: 2, category: "bakery" },
+    { name: "Eggs, dozen ", quantity: 2, category: "dairy" },
+    { name: "Bananas ", quantity: 6, category: "produce" },
+    { name: "Broccoli ", quantity: 3, category: "produce" },
+    { name: "Chicken breasts, 1 kg ", quantity: 1, category: "meat" },
+    { name: "Pasta sauce ", quantity: 3, category: "canned goods" },
+    { name: "Spaghetti, 454 g ", quantity: 2, category: "dry goods" },
+    { name: "Toilet paper, 12 pack ", quantity: 1, category: "household" },
+    { name: "Paper towels, 6 pack", quantity: 1, category: "household" },
+    { name: "Dish soap ", quantity: 1, category: "household" },
+    { name: "Hand soap ", quantity: 4, category: "household" },
   ];
 
   return (
--- app/week-3/page.tsx
+++ app/week-3/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
     <main className="min-h-screen bg-slate-100 p-10">
       <div className="max-w-2xl mx-auto">
         <h1 className="text-4xl font-bold text-center mb-10 text-indigo-700">
-          🛒 Shopping List
+           Shopping List
         </h1>
 
         <div className="bg-white rounded-2xl shadow-lg p-6">
