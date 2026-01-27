084acdea4c255f32d1bfb0b6c86c4de13a39ab6d
Asad Arif <Asad.Arif@edu.sait.ca>
2026-01-27 12:39:36 -0700

    done

--- app/page.js
+++ app/page.js
@@ -1,33 +1,19 @@
 import Link from "next/link";
 
-
-
-export default function Home() {
-return (
-<main className="p-10">
-<h1 className="text-3xl font-bold mb-6">Assignments</h1>
-
-
-<ul className="space-y-3">
-<li>
-<Link
-href="/week-4"
-className="text-blue-600 underline hover:text-blue-800"
->
-Week 4 Assignment
-</Link>
-</li>
-</ul>
-</main>
-);
-}
-
 export default function Page() {
   return (
-    <main>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+    <main className="p-10">
+      <h1 className="text-3xl font-bold mb-6">
+        CPRG 306: Web Development 2 - Assignments
+      </h1>
 
-      <Link href="/week-2">Go to Week 2</Link>
+      <ul className="space-y-3">
+        <li>
+          <Link href="/week-4" className="text-blue-600 underline">
+            Go to Week 4
+          </Link>
+        </li>
+      </ul>
     </main>
   );
 }
